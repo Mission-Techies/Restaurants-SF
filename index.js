cfe8e05bc0fcb85c78ad381851373874af34cb24
taha1548 <tahaberche@gmail.com>
2017-03-15 15:01:08 -0700

    i got the pic to display

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 var restaurants_data_url = 'https://api.airtable.com/v0/appFQlFxeZz0lJMJ7/Restaurants%20SF?api_key=keykETEaNX5SX8cZb';
 var restaurantsHTML = '';
 var restaurantsDiv = $('.restaurants');
-//var restaurants_Pictures = restaurants.fields['Pictures'];
+
 var renderrestaurantsData = function(data) {
   console.log(data);
   data.records.forEach(function(restaurant) {
-
+var restaurants_Pictures = restaurant.fields['Pictures'];
     restaurantsHTML += '<h2>' + restaurant.fields['Name'] + '</h2>';
     restaurantsHTML += '<h4>' + restaurant.fields['Address'] + '</h4>';
-    restaurantsHTML += '<p>' + restaurant.fields['phone number'] + '</p>';
-   restaurantsHTML += '<p>' + restaurant.fields['Rate'] + '</p>';
 
- restaurantsHTML += '<a href="' + restaurant.fields['Website'] + '">' + restaurant.fields['Website'] + '</a>';
-   restaurantsHTML += '<p>' + restaurant.fields['Hours'] + '</p>'
-    restaurantsHTML += '<a href="' + restaurant.fields['Menu'] + '">' + restaurant.fields['Menu'] + '</a>';
-    //restaurantsHTML += '<img  src =" ${Pictures.url}" >';
-  restaurantsHTML += restaurant.fields['Pictures'];
-    restaurantsHTML += '<hr />';
-  });
-  restaurantsDiv.html(restaurantsHTML);
+if (restaurant.fields['phone number']) {
+  restaurantsHTML += '<p>' + restaurant.fields['phone number'] + '</p>';
+}
+if (restaurant.fields['Rate']){
+   restaurantsHTML += '<p>' + restaurant.fields['Rate'] + '</p>';
 };
 
+
+if(restaurant.fields['Website']) {
+   restaurantsHTML += '<a href="' + restaurant.fields['Website'] + '">' + restaurant.fields['Website'] + '</a>';
+};
+
+if (restaurant.fields['Hours']) {
+  restaurantsHTML += `<p>${restaurant.fields['Hours']}</p>`;
+};
+
+
+
+    restaurantsHTML += '<a href="' + restaurant.fields['Menu'] + '">' + restaurant.fields['Menu'] + '</a>';
+
+
+  $.each(restaurants_Pictures, function(i, Pictures){
+    restaurantsHTML += `<img src="${Pictures.url}">`;
+    console.log(Pictures.url);
+    restaurantsHTML += '<hr />';
+  });
+
+  restaurantsDiv.html(restaurantsHTML);
+});
+}
 //
 $.getJSON(restaurants_data_url, renderrestaurantsData);
