48835cbd892318feac3e8143bf9f75abdbe0bc21
taha1548 <tahaberche@gmail.com>
2017-03-28 16:47:16 -0700

    adding cuisine

--- index.js
+++ index.js
@@ -3,44 +3,56 @@ var restaurantsHTML = '';
 var restaurantsDiv = $('.restaurants');
 
 var renderrestaurantsData = function(data) {
-  console.log(data);
+  // console.log(data);
   data.records.forEach(function(restaurant) {
-var restaurants_Pictures = restaurant.fields['Pictures'];
-    restaurantsHTML += '<h2>' + restaurant.fields['Name'] + '</h2>';
-    restaurantsHTML += '<h4>' + restaurant.fields['Address'] + '</h4>';
+//this code for american restaurants
 
-if (restaurant.fields['phone number']) {
-  restaurantsHTML += '<p>' + restaurant.fields['phone number'] + '</p>';
+  var restaurants_Pictures = restaurant.fields['Pictures'];
+
+  if(restaurant.fields['Name']) {
+
+      restaurantsHTML += '<div class="rest"><h2>' + restaurant.fields['Name'] + '</h2>';
+      restaurantsHTML += '<h4>' + restaurant.fields['Address'] + '</h4>';
+
+
+    if(restaurant.fields['Cuisine']) {
+      foodclass = restaurant.fields['Cuisine']
+      console.log(foodclass)
+      restaurantsHTML += '<h3 class="hidden">' + restaurant.fields['Cuisine'] + '</h3>';
+
+    }
+
+    if (restaurant.fields['phone number']) {
+      restaurantsHTML += '<p>' + restaurant.fields['phone number'] + '</p>';
+    }
+    if (restaurant.fields['Rate']){
+       restaurantsHTML += '<p>' + restaurant.fields['Rate'] + '</p>';
+     };
+
+    if(restaurant.fields['Website']) {
+       restaurantsHTML += '<a href="' + restaurant.fields['Website'] + '">' + restaurant.fields['Website'] + '</a>';
+    };
+
+    if (restaurant.fields['Hours']) {
+      restaurantsHTML += `<p>${restaurant.fields['Hours']}</p>`;
+    };
+
+    if (restaurant.fields['Menu']) {
+      restaurantsHTML += '<a href="' + restaurant.fields['Menu'] + '">' + restaurant.fields['Menu'] + '</a>';
+    };
 }
-if (restaurant.fields['Rate']){
-   restaurantsHTML += '<p>' + restaurant.fields['Rate'] + '</p>';
-};
-
-
-if(restaurant.fields['Website']) {
-   restaurantsHTML += '<a href="' + restaurant.fields['Website'] + '">' + restaurant.fields['Website'] + '</a>';
-};
-
-if (restaurant.fields['Hours']) {
-  restaurantsHTML += `<p>${restaurant.fields['Hours']}</p>`;
-};
-
-if (restaurant.fields['Menu']) {
-  restaurantsHTML += '<a href="' + restaurant.fields['Menu'] + '">' + restaurant.fields['Menu'] + '</a>';
-}
-
-
 
 
   $.each(restaurants_Pictures, function(i, Pictures){
   restaurantsHTML += "</br>";
     restaurantsHTML += `<img src="${Pictures.url}">`;
-    console.log(Pictures.url);
+    // console.log(Pictures.url);
     restaurantsHTML += '<hr />';
+  });
+  restaurantsHTML += '</div>';
   });
 
   restaurantsDiv.html(restaurantsHTML);
-});
 }
 //
 $.getJSON(restaurants_data_url, renderrestaurantsData);
